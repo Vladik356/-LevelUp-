72526816773f2102f28ccbc80967feb4e0a1ca66
vladi <vladik356@rambler.ru>
2021-09-16 22:55:52 +0300

    ВВОД

---
+++ js/myscrip.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$('.icons').slick({
+		//Basic settings
+		autoplay: true,
+		autoplaySpeed: 2000,
+		arrows: true,
+		swipeToSlide: true,
+		touchMove: true,
+		ltr: true,
+		infinite: true,
+		variableWidth: true,
+		dots: false,
+		pauseOnHover: false
+	});
+});
+
+
